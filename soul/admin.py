6cffbabaee904cf33d2ed17fdb44ed64c44f63ea
yixiao17 <yixiao17@vt.edu>
2022-05-04 19:31:39 -0400

    useless

--- soul/admin.py
+++ soul/admin.py
@@ -1,6 +1,3 @@
 from django.contrib import admin
-from import_export import resources
-from import_export.fields import Field
-from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 
